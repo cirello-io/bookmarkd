5f48cc7c394f7de9f854b75be185adf245b03725
Ulderico Cirello <u@cirello.eu>
2019-03-07 05:23:06 +0100

    bookmarkd/pkg/mail: use the first URL found when adding link

--- pkg/mail/mail.go
+++ pkg/mail/mail.go
@@ -84,10 +84,8 @@ func (u *user) extractLink(r io.Reader) {
 			return
 		}
 		urls := xurls.Strict().FindAllString(string(body), -1)
-		if len(urls) != 1 {
+		if len(urls) == 0 {
 			log.Println("cannot find link:", err)
-			log.Println("body")
-			log.Println(string(body))
 			return
 		}
 		bookmarkDAO := models.NewBookmarkDAO(u.backend.db)
